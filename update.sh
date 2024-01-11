3832658790c4d35f27730c9a7e20a7def6dbac80
MisterGrinvalds <ross.grinvalds@gmail.com>
2024-01-11 18:41:17 +0000

    write first shell script...

---
+++ update.sh
@@ -0,0 +1,22 @@
+source .env
+
+
+urlencode() {
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+}
+
+# Usage
+USER=$(urlencode "$USER")
+PASSWORD=$(urlencode "$PASSWORD")
+HOST=$(urlencode "$HOST")
+DOMAIN=$(urlencode "$DOMAIN")
+IP=$(curl -s http://myip.dnsomatic.com/)
+
+curl "http://$USER:$PASSWORD@updates.dnsomatic.com/nic/update?hostname=$HOST$DOMAIN&myip=$IP&wildcard=NOCHG&mx=NOCHG&backmx=NOCHG"
